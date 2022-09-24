0c77bb04f04c067b5fb45149ae639b46618c6aa0
Milan-Uy <uymillano@gmail.com>
2022-09-24 15:42:52 +0800

    table

--- project3.py
+++ project3.py
@@ -5,7 +5,7 @@ from prettytable import PrettyTable
 main_api = "https://www.mapquestapi.com/directions/v2/route?"
 key = "lUQgxATC6sYeGAogZ0A7vJd3ivsHP8Ju"
 
-myTable = PrettyTable(["Origin", "Destination","Distance (Miles)","Fuel Used (Gal)","Directions"])
+myTable = PrettyTable(["Origin", "Destination","Distance (Miles)", "Fuel Used (Gal)"])
 
 
 while True:
@@ -35,7 +35,7 @@ while True:
         for each in json_data["route"]["legs"][0]["maneuvers"]:
             print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
         print("=============================================\n")
-        myTable.add_row([(orig), (dest)])
+        myTable.add_row([(orig), (dest), str(json_data["route"]["distance"]), str(json_data["route"]["fuelUsed"])])
         print(myTable)
 
     elif json_status == 402:

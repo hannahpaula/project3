5b93db4163cda9bc934de3a8978f218af2d0d53e
Milan-Uy <uymillano@gmail.com>
2022-09-24 16:59:10 +0800

    comments

--- project3.py
+++ project3.py
@@ -5,13 +5,15 @@ from prettytable import PrettyTable
 main_api = "https://www.mapquestapi.com/directions/v2/route?"
 key = "lUQgxATC6sYeGAogZ0A7vJd3ivsHP8Ju"
 
+# table for headings
 myTable = PrettyTable(["Origin", "Destination","Distance (Miles)", "Distance (Kilometers)", "Fuel Used (Gal)", "Fuel Used (Ltr)"])
 
-
 while True:
+    # gets user input starting location
     orig = input("Starting Location: ")
     if orig == "quit" or orig == "q":
         break
+    # gets user input destination
     dest = input("Destination: ")
     if dest == "quit" or dest == "q":
         break
@@ -19,27 +21,20 @@ while True:
     print("URL: " + (url))
     json_data = requests.get(url).json()
     json_status = json_data["info"]["statuscode"]
+    # print the output if successful
     if json_status == 0:
         print("API Status: " + str(json_status) + " = A successful route call.\n")
         print("=============================================")
         # Outputs the data into a table
         myTable.add_row([(orig), (dest), str(json_data["route"]["distance"]), str("{:.2f}".format((json_data["route"]["distance"])*1.61)), str(json_data["route"]["fuelUsed"]), str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)) ])
         print(myTable)
-        # print("Directions from " + (orig) + " to " + (dest))
-        # print("Trip Duration: " + (json_data["route"]["formattedTime"]))
-        # miles and galon
-        # print("Miles: " + str(json_data["route"]["distance"]))
-        # print("Fuel Used (Gal): " + str(json_data["route"]["fuelUsed"]))
-        # kilometers and liters conversion
-        # print("Kilometers: " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
-        # print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
         print("=============================================")
         print("Directions:")
         for each in json_data["route"]["legs"][0]["maneuvers"]:
             print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
         print("=============================================\n")
         
-
+    # for errors
     elif json_status == 402:
         print("**********************************************")
         print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
